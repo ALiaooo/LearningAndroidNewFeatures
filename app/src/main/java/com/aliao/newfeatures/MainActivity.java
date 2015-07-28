a4882f5611c4c4128edf6018102a5fc325b638d5
阿廖 <liaolishuang@gmail.com>
2015-07-28 09:46:35 +0800

    Initial commit

---
+++ app/src/main/java/com/aliao/newfeatures/MainActivity.java
@@ -0,0 +1,116 @@
+package com.aliao.newfeatures;
+
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+
+/**
+ * 1.DrawLayout
+ * 2.FloatActionButton
+ * 3.Snackbar
+ * 4.CoordinatorLayout
+ * 5.AppBarLayout
+ * 6.TabLayout
+ * 7.NestedScrollView
+ * 8.CollapsingToolbarLayout
+ * 9.NavigationView
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton mFabBtn;
+    private CoordinatorLayout mRootLayout;
+    private Toolbar mToolbar;
+    private TabLayout mTabLayout;
+    private CollapsingToolbarLayout mCollapsingToolbarLayout;
+    private NavigationView mNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initToolbar();
+        initInstances();
+        //ddha
+    }
+
+    private void initToolbar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+    }
+
+    private void initInstances() {
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mRootLayout = (CoordinatorLayout) findViewById(R.id.rootLayout);
+//        mTabLayout = (TabLayout) findViewById(R.id.tabLayout);
+//        mTabLayout.addTab(mTabLayout.newTab().setText("Tab 1"));
+//        mTabLayout.addTab(mTabLayout.newTab().setText("Tab 2"));
+//        mTabLayout.addTab(mTabLayout.newTab().setText("Tab 3"));
+        mCollapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbarLayout);
+        mCollapsingToolbarLayout.setTitle("Design Support View");
+        mFabBtn = (FloatingActionButton) findViewById(R.id.fabBtn);
+        mFabBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(mRootLayout, "show sanckbar", Snackbar.LENGTH_SHORT).setAction("UNDO", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                }).show();
+            }
+        });
+
+        mNavigationView = (NavigationView) findViewById(R.id.navigation);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                switch (id){
+                    case R.id.navItem1:
+                        break;
+                    case R.id.navItem2:
+                        break;
+                    case R.id.navItem3:
+                        break;
+                    case R.id.navItem4:
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/aliao/newfeatures/activity/DoubanMainActivity.java
@@ -0,0 +1,18 @@
+package com.aliao.newfeatures.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.aliao.newfeatures.R;
+
+/**
+ * Created by 丽双 on 2015/7/28.
+ */
+public class DoubanMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doubanmain);
+    }
+}
